dc001fd2f1ce5977b80d72a2ecbbd6d9fc93e5e1
victor <vemb96@gmail.com>
2019-02-14 17:54:46 -0300

    actualizar tabla

--- calendar.js
+++ calendar.js
@@ -10,19 +10,37 @@ class calendar{
       dentro de cada elemento de la tabla para eso usamos calendarTable */
     this.calendarTable = null; 
     this.buildTable();
+    this. updateTable();
   }
 
   //actualizar la tabla dependiendo del mes a visualizar
   
   updateTable(){
-    this.claculateDay();
+    this.calculateDay();
+    let firstDayInWeek = this.monthStar.getDay(); //dia de la semanas
+    let trs = this.calendarTable.querySelectorAll('tr')
+
+    for (let i = 0; i <= 5; i++) {
+      let tr = trs[i]
+      let tds = tr.querySelectorAll('td')
+
+      for (let j = 0; j < 7 ; j++) {
+        let td = tds[j]
+        let day = (i*7)+ j
+        if (day >= firstDayInWeek && (day - firstDayInWeek)< this.monthDays)
+        td.innerHTML = day -firstDayInWeek + 1
+        
+      } 
+    }
+
   };
 
   //calcular cuando comienza y termina el mes y cuantos dias tiene
   calculateDay(){
-    this.monthStar = new Date(this.date.getFullYear(),this.date.getMonth(),1 )
-    this.monthEnd = new Date(this.date.getFullYear(),this.date.getMonth()+1,1 )
+    this.monthStar = new Date(this.date.getFullYear(),this.date.getMonth(), 1)
+    this.monthEnd = new Date(this.date.getFullYear(),this.date.getMonth()+1, 1)
     this.monthDays =  Math.floor((this.monthEnd - this.monthStar) / (1000 * 60 *60 * 24))
+    //uso floor por que en algunos meses la resta da decimales
   };
 
   buildTable(){
@@ -37,15 +55,15 @@ class calendar{
     }
     /*hasta 6 por que si un mes empieza el sabado
      puede necesitar mas de 5 filas donde se coloquen los elemntos */
-    for (let i = 0; i <= 6; i++) {
+    for (let i = 0; i <= 5; i++) {
       let tr = document.createElement('tr')
-      for (let j = 0; j <= 6; j++) {
+      for (let j = 0; j < 7; j++) {
         let td = document.createElement('td')
         tr.appendChild(td)
       } 
       table.appendChild(tr)
     }
-     
+    this.calendarTable = table;
     table.appendChild(thead)
     this.container.appendChild(table)
   }
