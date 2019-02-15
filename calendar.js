38994a4381fd9eb84e83e48df737cb73ac36f8c6
victor <vemb96@gmail.com>
2019-02-15 15:52:12 -0300

    botones/ funcionalidad botones/ css

--- calendar.js
+++ calendar.js
@@ -1,46 +1,104 @@
 const days =['D','L','M','X','J','V','S']
+const months=['Enero','Febrero','Marzo','Abril','Mayo','junio','Julio','agosto','Septiembre', 'Octubre','Noviembre', 'Diciembre'];
+
+
+                  
+
 
 class calendar{
   constructor(options){
+    
+   
     /*ingresar date es opcional por si se quiere que el
      calendario imprima una fecha diferente a la actual*/ 
     this.date = options.date || new Date();
+    this.today = new Date();
     this.container = options.container;
     /* la tabla se va generar una ves luego cuando se cambie de mes solo se modificaran los valores
       dentro de cada elemento de la tabla para eso usamos calendarTable */
     this.calendarTable = null; 
+    
     this.buildTable();
-    this. updateTable();
+    this.updateTable();
+    this.createButtons();
+    this.holiday();
   }
+  //siguiente mes
+  next(){
+    if (this.date.getMonth == 11){
+      this.date = new Date(this.date.getFullYear()+1, 0, 1) 
+    }
+    else{
+      this.date = new Date(this.date.getFullYear(),this.date.getMonth() + 1, 1)
+      console.log('aqui');
+      console.log(this.date);
+    }
+  this.updateTable();
+  this.createButtons();
+  }
+  //anterior mes
+  prev(){
+    if (this.date.getMonth == 0){
+      this.date = new Date(this.date.getFullYear()-1, 11, 1) 
+      console.log("hola");
+    }
+    else{
+      this.date = new Date(this.date.getFullYear(),this.date.getMonth() - 1, 1)
+    }
+    this.updateTable();
+    this.createButtons();
+  }
+  //creo y agrego funcionalidad a los botones
+  createButtons(){
+    this.buttons = {};
 
+    this.buttons.next = document.createElement('div')
+    this.buttons.prev = document.createElement('div')
+
+    this.buttons.next.classList.add('next-arrow')
+    this.buttons.prev.classList.add('prev-arrow')
+    
+    this.container.querySelector('.relative').appendChild(this.buttons.next)
+    this.container.querySelector('.relative').appendChild(this.buttons.prev)
+    
+    this.buttons.next.addEventListener('click',()=> this.next())  
+    this.buttons.prev.addEventListener('click',()=> this.prev())
+  }
   //actualizar la tabla dependiendo del mes a visualizar
-  
   updateTable(){
     this.calculateDay();
     let firstDayInWeek = this.monthStar.getDay(); //dia de la semanas
     let trs = this.calendarTable.querySelectorAll('tr')
 
-    for (let i = 0; i <= 5; i++) {
+    for (let i = 0; i <= 5; i++) {//filas semanas
       let tr = trs[i]
       let tds = tr.querySelectorAll('td')
 
-      for (let j = 0; j < 7 ; j++) {
+      for (let j = 0; j < 7 ; j++) {//columnas dias de semana
         let td = tds[j]
         let day = (i*7)+ j
-        if (day >= firstDayInWeek && (day - firstDayInWeek)< this.monthDays)
-        td.innerHTML = day -firstDayInWeek + 1
-        
+        if (day >= firstDayInWeek && (day - firstDayInWeek)< this.monthDays){
+          td.innerHTML = day - firstDayInWeek + 1}
+        else{
+          td.innerHTML = ''
+        }
+        if ((day - firstDayInWeek + 1) == this.today.getDate() && this.today.getFullYear() == this.date.getFullYear() && months[this.date.getMonth()] == months[this.today.getMonth()]){
+          td.classList.add('today')}
+        else{
+          td.classList.remove('today')
+        }
       } 
     }
-
+    this.container.querySelector('header').innerHTML = `${months[this.date.getMonth()]}, ${this.date.getFullYear()}`
+    this.container.querySelector('header').classList.add('relative')
   };
 
   //calcular cuando comienza y termina el mes y cuantos dias tiene
   calculateDay(){
     this.monthStar = new Date(this.date.getFullYear(),this.date.getMonth(), 1)
     this.monthEnd = new Date(this.date.getFullYear(),this.date.getMonth()+1, 1)
-    this.monthDays =  Math.floor((this.monthEnd - this.monthStar) / (1000 * 60 *60 * 24))
-    //uso floor por que en algunos meses la resta da decimales
+    this.monthDays =  Math.floor((this.monthEnd - this.monthStar) / (1000 * 60 *60 * 24)) //llevo el resultado de milisegundos a dias
+    //uso floor por que en algunos meses el resultado da decimales
   };
 
   buildTable(){
@@ -48,24 +106,60 @@ class calendar{
     let thead = document.createElement('thead')
     
 
-    for(let i=0; i<7; i++){
+    for(let i=0; i<7; i++){//head de la tabla(dias de semana)
       let td = document.createElement('td')
       td.innerHTML = days[i]
       thead.appendChild(td)
     }
     /*hasta 6 por que si un mes empieza el sabado
      puede necesitar mas de 5 filas donde se coloquen los elemntos */
-    for (let i = 0; i <= 5; i++) {
+    for (let i = 0; i <= 5; i++) {// filas de semana
       let tr = document.createElement('tr')
-      for (let j = 0; j < 7; j++) {
+      for (let j = 0; j < 7; j++) {// dias de semana
         let td = document.createElement('td')
         tr.appendChild(td)
       } 
       table.appendChild(tr)
     }
+    
     this.calendarTable = table;
+
     table.appendChild(thead)
-    this.container.appendChild(table)
+    
+    let body = document.createElement('div')
+    body.classList.add('body')
+    body.appendChild(table)
+    
+    this.container.appendChild(document.createElement('header'))
+    this.container.appendChild(body)
+    this.container.classList.add('shipnow-calendar')
+  }
+
+  holiday(){
+    let firstDayInWeek = this.monthStar.getDay();
+    let trs = this.calendarTable.querySelectorAll('tr')
+    
+    fetch('http://nolaborables.com.ar/api/v2/feriados/2019')
+    .then(res =>  res.json())
+    .then(holiday => {
+      for (let i = 0; i <= 5; i++) {//filas semanas
+        let tr = trs[i]
+        let tds = tr.querySelectorAll('td')
+  
+        for (let j = 0; j < 7 ; j++) {//columnas dias de semana
+          let td = tds[j]
+          let day = (i*7)+ j
+
+          holiday.forEach(holiday => {
+            if ((day - firstDayInWeek + 1) == holiday.dia) {
+              td.classList.add('holiday')}
+            else{
+              td.classList.remove('holiday')
+            }
+          });
+        } 
+      }
+    });
   }
 
 }
